b3ab6707e8c7da82be344d120582a101777ad8c8
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-01 20:29:26 -0300

    feat: add routes to render different components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import SignupScreen from './pages/SignupScreen';
+
 function App() {
-  return <div>Home screen</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home screen</div>} />
+        <Route path="/signup" element={<SignupScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
