77166c15c626c6c6de248994980f799675fa78f1
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-01 20:45:59 -0300

    feat: add image to screen

--- src/pages/SignupScreen.tsx
+++ src/pages/SignupScreen.tsx
@@ -1,5 +1,14 @@
+import PageContainer from '../components/styles/PageContainer';
+import SideImage from '../components/styles/SideImage';
+
+import image from '../assets/cthulhu_wants_you.jpeg';
+
 function SignupScreen() {
-  return <>Signup page</>;
+  return (
+    <PageContainer>
+      <SideImage src={image} alt="Cthulhu wants YOU" />
+    </PageContainer>
+  );
 }
 
 export default SignupScreen;
