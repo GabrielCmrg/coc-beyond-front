88583f58c5c4bab26d21420ef05a975f851d2fbf
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-02 11:16:15 -0300

    feat: add HomeScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import './assets/reset.css';
 import GlobalStyles from './themes/GlobalStyles';
 
 import SignupScreen from './pages/SignupScreen';
+import HomeScreen from './pages/HomeScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<div>Home screen</div>} />
+        <Route path="/" element={<HomeScreen />} />
         <Route path="/signup" element={<SignupScreen />} />
       </Routes>
     </BrowserRouter>
